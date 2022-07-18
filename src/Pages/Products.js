75f313bbc37377fde3e787284461d39b5296095c
harllogeen <harllogeen2020@gmail.com>
2022-07-18 13:52:59 +0100

    newer

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css'
+import Footer from '../Components/Footer';
 
 const Products = () => {
   return (
     <div>
       <h1 className='products'>PRODUCTS</h1>
+      <Footer />
     </div>
   )
 }
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css'
+import Footer from '../Components/Footer';
 
 const Services = () => {
   return (
     <div>
       <h1 className='services'>SERVICES</h1>
+      <Footer />
     </div>
   )
 }
